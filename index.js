96dee74436cc4ba80ec0cd8de6449a84da4aeaff
Cole Bianchi <RaviusSky@users.noreply.github.com>
2020-04-25 04:03:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 var http = require("http")
+var https = require("https")
 var fs = require("fs")
 
 port = process.env.PORT
+
+var client_id = "30860a4d8522424a887e9f91c975588f";
 
 http.createServer(function(req, res)
 {
 	console.log(req.url.split("?")[0])
 	if (req.url.split("?")[0] == "/login")
 	{
-		var client_id = "30860a4d8522424a887e9f91c975588f";
+		
 		var redirect_uri = "https://spotifywebplayer.herokuapp.com"
 		var scopes = 'streaming user-read-email user-read-private';
 		res.writeHead(301, { Location: 'https://accounts.spotify.com/authorize' +
@@ -16,6 +19,39 @@ http.createServer(function(req, res)
 		'&client_id=' + client_id +
 		(scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
 		'&redirect_uri=' + encodeURIComponent(redirect_uri)});
+	}
+	if (req.url.split("?")[0] == "/token")
+	{
+		var authCode = url.parse(req.url, true).query.code
+		
+		var requestData = JSON.stringify({
+			grant_type: "authorization_code",
+			code: authCode,
+			redirect_uri: "https://spotifywebplayer.herokuapp.com"
+		})
+		
+		const tokenOptions = {
+			hostname: "accounts.spotify.com",
+			port: 443,
+			path: "/api/token",
+			method: "POST",
+			headers: {
+				"Authorization": "Basic " + btao(client_id),
+				"Content-Type": "application/x-www-form-urlencoded",
+			}
+			
+			const tokenReq = https.request(tokenOptions, tokenRes => {
+				console.log("Token request status code: ${tokenRes.statusCode}")
+			
+				tokenRes.on("data", d => {
+					process.stdout.write(d)
+				})
+			})
+	
+			tokenReq.on("error", error => {
+				console.error(error)
+			})
+		}
 	}
 	if (req.url.split("?")[0] == "/")
 	{
