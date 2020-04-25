68cc050e19df2c0b10651f25dbb12aceb91d3087
Cole Bianchi <RaviusSky@users.noreply.github.com>
2020-04-25 03:14:17 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ http.createServer(function(req, res)
 	if (req.url == "/login")
 	{
 		var client_id = "30860a4d8522424a887e9f91c975588f";
-		var redirect_uri = "https://spotifywebplayer.ravius.info"
+		var redirect_uri = "https://spotifywebplayer.herokuapp.com"
 		var scopes = 'user-modify-playback-state streaming user-read-email user-read-private';
 		res.writeHead(301, { Location: 'https://accounts.spotify.com/authorize' +
 		'?response_type=code' +
