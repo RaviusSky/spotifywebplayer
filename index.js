af2aba8c9deeecf73af714dec11bfcea25756f76
Cole Bianchi <RaviusSky@users.noreply.github.com>
2020-04-25 22:17:37 -0400

    Update index.js

--- index.js
+++ index.js
@@ -122,7 +122,7 @@ http.createServer(function(req, res)
 						res.write("DEVICE_NOT_FOUND")
 						res.end()
 					}
-					else if (getRes.statusCode == "200" || getRes.statusCode = "204")
+					else if (getRes.statusCode == "200" || getRes.statusCode == "204")
 					{
 						res.write("SONG_PAUSED")
 						res.end()
